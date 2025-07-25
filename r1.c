204dc2a3af38b0fcd7e45614a6dd3106a44c667c
roh6080 <rohitsuryask2006@gmail.com>
2025-07-25 23:23:04 +0530

    Add files via upload

---
+++ r1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(){
+int num1,num2,sum;
+printf("enter first number");
+scanf("%d",&num1);
+printf("enter second number");
+scanf("%d",&num2);
+sum=num1+num2;
+printf("sum 2",num1,num2,sum);
+return 0;
+}
