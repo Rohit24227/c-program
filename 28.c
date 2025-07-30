6952f49b7735e7a9e4e7dee64075f7fb8df18f66
rohit-227 <rohitsuryask2006@gmail.com>
2025-07-31 00:03:27 +0530

    Add files via upload

---
+++ 28.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main() {
+int mark;
+printf("enter the mark");
+scanf ("%d",&mark);
+if (mark>=90)
+printf("grade :A\n");
+else if (mark>=80)
+printf("grade :B\n");
+else if (mark>=70)
+printf("grade :C\n");
+else if (mark>=60)
+printf("grade :D\n");
+else if (mark>=50);
+printf("grade:E\n");
+else ;
+printf("grade:F(Fail)/n");
+return 0;
+}
