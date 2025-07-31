37243ef7d48c9d0b4d69ab9770cdbff369eea617
rohit-227 <rohitsuryask2006@gmail.com>
2025-08-01 00:15:36 +0530

    Add files via upload

---
+++ 32.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+int units;
+float bill = 0;
+printf("Enter total units consumed: ");
+scanf("%d", &units);
+if(units <= 100) {
+bill = units * 1.50;
+}
+else if(units <= 200) {
+bill = 100 * 1.50 + (units - 100) * 2.00;
+}
+else if(units <= 300) {
+bill = 100 * 1.50 + 100 * 2.00 + (units - 200) * 3.00;
+}
+else {
+bill = 100 * 1.50 + 100 * 2.00 + 100 * 3.00 + (units - 300) * 5.00;
+}
+printf("Electricity Bill = ₹%.2f\n", bill);
+return 0;
+}
